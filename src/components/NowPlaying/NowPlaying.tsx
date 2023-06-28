8d3b9061e94ba9fdb5ff91fc809386af83020c8b
damdeez <damir.hara@gmail.com>
2023-06-27 18:05:52 -0600

    more fixes in NowPlaying

--- src/components/NowPlaying/NowPlaying.tsx
+++ src/components/NowPlaying/NowPlaying.tsx
@@ -1,28 +1,37 @@
 import './nowplaying.scss';
 
-interface Image {
+interface AlbumArtwork {
   size: string;
+  '#text': string;
+}
+
+interface Artist {
+  mbid: string;
+  '#text': string;
 }
 
 const data = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=damdeez&api_key=${import.meta.env.PUBLIC_LASTFM_KEY}&limit=1&format=json`).then((response) => response.json());
 
 const spotifyDataRecent = data.recenttracks.track[0];
+const nowPlaying: boolean = spotifyDataRecent?.['@attr']?.nowplaying ?? false;
+const albumArtwork: AlbumArtwork[] = spotifyDataRecent.image;
+const artist: Artist = spotifyDataRecent.artist;
+const songName: string = spotifyDataRecent.name;
 
 const NowPlaying = () => {
   return (
     <div className="now-playing">
       {spotifyDataRecent && (
         <>
-          <span className="now-listening">#NowListening: </span>
-          {spotifyDataRecent.image
+          <span className="now-listening">{nowPlaying ? '🎧 #NowListening:' : '🎵 Last played:'} </span>
+          {albumArtwork
             && (
               <span className="album-cover">
-                <img alt="album-cover" src={spotifyDataRecent.image.find((image: Image) => image.size === 'medium')['#text']} />
+                <img alt="album-cover" src={albumArtwork.find((image: AlbumArtwork) => image.size === 'medium')?.['#text']} />
               </span>
             )}
-          {spotifyDataRecent.artist
-            && <span className="artist">{spotifyDataRecent.artist['#text']}</span>}
-          {spotifyDataRecent.name && <span className="song-name"> - {spotifyDataRecent.name}</span>}
+          {artist && <span className="artist">{artist['#text']}</span>}
+          {songName && <span className="song-name"> - {songName}</span>}
         </>
       )}
     </div>
