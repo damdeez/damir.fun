ca09e6c38124d0297bffe9cf9051937a81426c93
damdeez <damir.hara@gmail.com>
2023-07-11 10:18:03 -0600

    added in Threads link and upped version of Astro

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => (
     <img className="damir-img" src={damirJpg} alt="Damir" />
     <section className="main-text">
       <h1 className="intro-paragraph">{'>'} Hi, my name is Damir.</h1>
-      <h3>I am a Software Engineer focused on building aesthetically pleasing Web products. You can check out my resume <a href="/resume">here</a>. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:damir.hara@gmail.com">email</a>. ☕️
+      <h3>I am a Software Engineer focused on building aesthetically pleasing Web products. You can check out my resume <a href="/resume">here</a>. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.threads.net/@damdeez">Threads</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:damir.hara@gmail.com">email</a>. ☕️
       </h3>
     </section>
   </div>
