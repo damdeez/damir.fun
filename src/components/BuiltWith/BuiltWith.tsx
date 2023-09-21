5579837bda919a05c50f81d2539e21094ce53768
damdeez <damir.hara@gmail.com>
2023-09-20 21:25:57 -0600

    added in BuiltWith component, fixing some styling

--- src/components/BuiltWith/BuiltWith.tsx
+++ src/components/BuiltWith/BuiltWith.tsx
@@ -1,7 +1,7 @@
 import './built.scss';
 
 const BuiltWith = () => (
-  <span className="cool-fact">Built with <a href="https://astro.build/">Astro</a>! 🚀</span>
+  <span className="built-with">👾 Built with{' '}<a href="https://astro.build/"> Astro</a>! 🚀</span>
 );
 
 export default BuiltWith;
