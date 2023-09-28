669db3cbb226d3806eb47a5a98a18247e7495e05
damdeez <damir.hara@gmail.com>
2023-09-27 22:15:22 -0600

    upped pkgs and updated links

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,8 +5,8 @@ const Main = () => (
     <img className="damir-img" src="/images/damir.jpg" alt="Picture of Damir" />
     <section className="main-text">
       <h1 className="intro-paragraph">{'>'} Hi, my name is Damir.</h1>
-      <h2>I am a Software Engineer focused on building aesthetically pleasing Web products. You can check out my <a href="/resume">resume</a> 👈 there. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.threads.net/@damdeez">Threads</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:damir.hara@gmail.com">email</a>. ☕️
-      </h2>
+      <p className="main-paragraph">I am a Software Engineer focused on building aesthetically pleasing Web products. You can check out my <a href="/resume">resume</a> 👈 there. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:damir.hara@gmail.com">email</a>. ☕️
+      </p>
     </section>
   </div>
 );
