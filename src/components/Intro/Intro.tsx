070334a02e93a456251f235e46190a4c729ba15f
damdeez <damir.hara@gmail.com>
2024-05-24 16:12:16 -0600

    updated some files

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,14 @@
+import './intro.scss';
+
+const Intro = () => (
+  <div className="main">
+    <img className="damir-img" src="/images/damir.jpg" alt="Picture of Damir" />
+    <section className="main-text">
+      <h1 className="intro-paragraph">{'>'} Hi, my name is Damir.</h1>
+      <p className="main-paragraph">I am a Software Engineer focused on building aesthetically pleasing products. You can check out my <a href="/resume">resume</a> 👈 there. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:damir.hara@gmail.com">email</a>. ☕️
+      </p>
+    </section>
+  </div>
+);
+
+export default Intro;
