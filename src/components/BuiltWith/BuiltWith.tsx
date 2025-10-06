cea0f8ae574a230d1c25988b7b4028082328f470
damdeez <damir.hara@gmail.com>
2025-10-05 22:43:14 -0600

    convert files into .astro

--- src/components/BuiltWith/BuiltWith.tsx
+++
@@ -1,7 +0,0 @@
-import './built.scss';
-
-const BuiltWith = () => (
-  <span className="built-with">👾 Built with{' '}<a href="https://astro.build/" rel="noopener noreferrer"> Astro</a>! 🚀</span>
-);
-
-export default BuiltWith;
